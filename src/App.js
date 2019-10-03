2026a33a8ee012ecd8ccfa556bd00eb3c0020ccb
andoreyrich <andoreyrich@gmail.com>
2019-10-03 09:27:03 +0200

    add sequence

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
+    tasks = [
+        {title: 'CSS', isDone: true},
+        {title: 'JS', isDone: false},
+        {title: 'ReactJS', isDone: false},
+        {title: 'Patterns', isDone: true},
+
+    ];
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader/>
-                    <TodoListTasks/>
-                    <TodoListFooter/>
+                    <TodoListHeader />
+                    <TodoListTasks tasks={this.tasks}/>
+                    <TodoListFooter />
                 </div>
             </div>
         );
--- src/TodoListTasks.jsx
+++ src/TodoListTasks.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
+import TodoListTask from "./TodoListTask";
 
 class TodoListTasks extends React.Component {
     render = () => {
         return (
                 <div className="todoList-tasks">
-                    <div className="todoList-task">
-                        <input type="checkbox" checked={true}/>
-                        <span>CSS</span>
-                    </div>
-                    <div className="todoList-task">
-                        <input type="checkbox" checked={false}/>
-                        <span>JS</span>
-                    </div>
-                    <div className="todoList-task">
-                        <input type="checkbox" checked={false}/>
-                        <span>ReactJS</span>
-                    </div>
-                    <div className="todoList-task">
-                        <input type="checkbox" checked={true}/>
-                        <span>Patterns</span>
-                    </div>
+                    <TodoListTask title={this.props.tasks[0].title} isDone={this.props.tasks[0].isDone}/>
+                    <TodoListTask title={this.props.tasks[1].title} isDone={this.props.tasks[1].isDone}/>
+                    <TodoListTask title={this.props.tasks[2].title} isDone={this.props.tasks[2].isDone}/>
+                    <TodoListTask title={this.props.tasks[3].title} isDone={this.props.tasks[3].isDone}/>
                 </div>
         );
     }
